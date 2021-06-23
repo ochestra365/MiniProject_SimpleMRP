6a5d168df202480da83812b8673d09aa86259923
ochestra365 <ochestra365@naver.com>
2021-06-23 10:32:10 +0900

    commit

---
+++ MRPApp/DeviceSubApp/FrmMain.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace DeviceSubApp
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtConnectionString = new System.Windows.Forms.TextBox();
+            this.TxtClientID = new System.Windows.Forms.TextBox();
+            this.TxtSubscriptionTopic = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connection String";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Client ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Subscription Topic";
+            // 
+            // TxtConnectionString
+            // 
+            this.TxtConnectionString.Location = new System.Drawing.Point(201, 32);
+            this.TxtConnectionString.Name = "TxtConnectionString";
+            this.TxtConnectionString.Size = new System.Drawing.Size(451, 21);
+            this.TxtConnectionString.TabIndex = 1;
+            this.TxtConnectionString.Text = "210.119.12.99";
+            // 
+            // TxtClientID
+            // 
+            this.TxtClientID.Location = new System.Drawing.Point(201, 78);
+            this.TxtClientID.Name = "TxtClientID";
+            this.TxtClientID.Size = new System.Drawing.Size(451, 21);
+            this.TxtClientID.TabIndex = 1;
+            this.TxtClientID.Text = "MACHINE01";
+            // 
+            // TxtSubscriptionTopic
+            // 
+            this.TxtSubscriptionTopic.Location = new System.Drawing.Point(201, 122);
+            this.TxtSubscriptionTopic.Name = "TxtSubscriptionTopic";
+            this.TxtSubscriptionTopic.Size = new System.Drawing.Size(451, 21);
+            this.TxtSubscriptionTopic.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(981, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1080, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(67, 160);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1088, 472);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 661);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TxtSubscriptionTopic);
+            this.Controls.Add(this.TxtClientID);
+            this.Controls.Add(this.TxtConnectionString);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "IoT Device Subscriber";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtConnectionString;
+        private System.Windows.Forms.TextBox TxtClientID;
+        private System.Windows.Forms.TextBox TxtSubscriptionTopic;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
