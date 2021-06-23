3701f0a05c9b78e15b425a035e78958d52e94284
ochestra365 <ochestra365@naver.com>
2021-06-23 10:50:28 +0900

    commit

--- MRPApp/DeviceSubApp/FrmMain.Designer.cs
+++ MRPApp/DeviceSubApp/FrmMain.Designer.cs
@@ -29,101 +29,114 @@ namespace DeviceSubApp
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.TxtConnectionString = new System.Windows.Forms.TextBox();
             this.TxtClientID = new System.Windows.Forms.TextBox();
             this.TxtSubscriptionTopic = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.BtnConnect = new System.Windows.Forms.Button();
+            this.BtnDisconnect = new System.Windows.Forms.Button();
+            this.RtbSubscr = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("나눔고딕", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label1.Location = new System.Drawing.Point(65, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(105, 12);
+            this.label1.Size = new System.Drawing.Size(256, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "Connection String";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(118, 81);
+            this.label2.Font = new System.Drawing.Font("나눔고딕", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(190, 77);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 12);
+            this.label2.Size = new System.Drawing.Size(131, 31);
             this.label2.TabIndex = 0;
             this.label2.Text = "Client ID";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(65, 131);
+            this.label3.Font = new System.Drawing.Font("나눔고딕", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(61, 123);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(111, 12);
+            this.label3.Size = new System.Drawing.Size(260, 31);
             this.label3.TabIndex = 0;
             this.label3.Text = "Subscription Topic";
             // 
             // TxtConnectionString
             // 
-            this.TxtConnectionString.Location = new System.Drawing.Point(201, 32);
+            this.TxtConnectionString.Font = new System.Drawing.Font("나눔고딕", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TxtConnectionString.Location = new System.Drawing.Point(331, 32);
             this.TxtConnectionString.Name = "TxtConnectionString";
-            this.TxtConnectionString.Size = new System.Drawing.Size(451, 21);
+            this.TxtConnectionString.Size = new System.Drawing.Size(451, 39);
             this.TxtConnectionString.TabIndex = 1;
             this.TxtConnectionString.Text = "210.119.12.99";
             // 
             // TxtClientID
             // 
-            this.TxtClientID.Location = new System.Drawing.Point(201, 78);
+            this.TxtClientID.Font = new System.Drawing.Font("나눔고딕", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TxtClientID.Location = new System.Drawing.Point(331, 74);
             this.TxtClientID.Name = "TxtClientID";
-            this.TxtClientID.Size = new System.Drawing.Size(451, 21);
+            this.TxtClientID.Size = new System.Drawing.Size(451, 39);
             this.TxtClientID.TabIndex = 1;
-            this.TxtClientID.Text = "MACHINE01";
+            this.TxtClientID.Text = "SUBSCR01";
             // 
             // TxtSubscriptionTopic
             // 
-            this.TxtSubscriptionTopic.Location = new System.Drawing.Point(201, 122);
+            this.TxtSubscriptionTopic.Font = new System.Drawing.Font("나눔고딕", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TxtSubscriptionTopic.Location = new System.Drawing.Point(331, 122);
             this.TxtSubscriptionTopic.Name = "TxtSubscriptionTopic";
-            this.TxtSubscriptionTopic.Size = new System.Drawing.Size(451, 21);
+            this.TxtSubscriptionTopic.Size = new System.Drawing.Size(451, 39);
             this.TxtSubscriptionTopic.TabIndex = 1;
+            this.TxtSubscriptionTopic.Text = "factory1/machine1/data/";
             // 
-            // button1
+            // BtnConnect
             // 
-            this.button1.Location = new System.Drawing.Point(981, 120);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.BtnConnect.Font = new System.Drawing.Font("나눔고딕", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BtnConnect.Location = new System.Drawing.Point(788, 111);
+            this.BtnConnect.Name = "BtnConnect";
+            this.BtnConnect.Size = new System.Drawing.Size(179, 43);
+            this.BtnConnect.TabIndex = 2;
+            this.BtnConnect.Text = "Connect";
+            this.BtnConnect.UseVisualStyleBackColor = true;
+            this.BtnConnect.Click += new System.EventHandler(this.BtnConnect_Click);
             // 
-            // button2
+            // BtnDisconnect
             // 
-            this.button2.Location = new System.Drawing.Point(1080, 120);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button1";
-            this.button2.UseVisualStyleBackColor = true;
+            this.BtnDisconnect.Font = new System.Drawing.Font("나눔고딕", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BtnDisconnect.Location = new System.Drawing.Point(973, 108);
+            this.BtnDisconnect.Name = "BtnDisconnect";
+            this.BtnDisconnect.Size = new System.Drawing.Size(182, 46);
+            this.BtnDisconnect.TabIndex = 2;
+            this.BtnDisconnect.Text = "Disconnect";
+            this.BtnDisconnect.UseVisualStyleBackColor = true;
+            this.BtnDisconnect.Click += new System.EventHandler(this.BtnDisconnect_Click);
             // 
-            // richTextBox1
+            // RtbSubscr
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(67, 160);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(1088, 472);
-            this.richTextBox1.TabIndex = 3;
-            this.richTextBox1.Text = "";
+            this.RtbSubscr.Font = new System.Drawing.Font("나눔고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.RtbSubscr.Location = new System.Drawing.Point(67, 160);
+            this.RtbSubscr.Name = "RtbSubscr";
+            this.RtbSubscr.Size = new System.Drawing.Size(1088, 472);
+            this.RtbSubscr.TabIndex = 3;
+            this.RtbSubscr.Text = "";
             // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1184, 661);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.RtbSubscr);
+            this.Controls.Add(this.BtnDisconnect);
+            this.Controls.Add(this.BtnConnect);
             this.Controls.Add(this.TxtSubscriptionTopic);
             this.Controls.Add(this.TxtClientID);
             this.Controls.Add(this.TxtConnectionString);
@@ -131,6 +144,7 @@ namespace DeviceSubApp
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "FrmMain";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
@@ -148,9 +162,9 @@ namespace DeviceSubApp
         private System.Windows.Forms.TextBox TxtConnectionString;
         private System.Windows.Forms.TextBox TxtClientID;
         private System.Windows.Forms.TextBox TxtSubscriptionTopic;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button BtnConnect;
+        private System.Windows.Forms.Button BtnDisconnect;
+        private System.Windows.Forms.RichTextBox RtbSubscr;
     }
 }
 
--- MRPApp/DeviceSubApp/FrmMain.cs
+++ MRPApp/DeviceSubApp/FrmMain.cs
@@ -16,5 +16,15 @@ namespace DeviceSubApp
         {
             InitializeComponent();
         }
+
+        private void BtnConnect_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnDisconnect_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
