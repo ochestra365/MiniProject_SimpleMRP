a561f12d44d3d384721f5ca60fad338fb2573801
ochestra365 <ochestra365@naver.com>
2021-06-24 12:37:35 +0900

    commit

--- MRPApp/MRPApp/Logic/DataAccess.cs
+++ MRPApp/MRPApp/Logic/DataAccess.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MRPApp.Model;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity.Migrations;
 using System.Linq;
@@ -9,6 +10,29 @@ namespace MRPApp.Logic
 {
     public class DataAccess
     {
-        
+        //셋팅 테이블에서 데이터 가져오기
+        internal static List<Settings> GetSettings()//internal 같은 어쎔블리안에서는 public으로 작동하니까 internal
+        {
+            List<Settings> settings;//Settings 테이블이랑 매칭이 되어 있다.
+            using (var ctx=new MRPEntities())//SQL서버 직접 연결 시 커넥션스트링과 같은 역할을 한다고 보면 된다. MRPEntities는 연결과 같다
+            {
+                settings = ctx.Settings.ToList();
+            }
+            //커넥션이 있을 때 커넥션을 닫아주는 역할을 한다. using을 하며 한 트랜잭션하에서 문제가 발생한다. 안정성이 좋다.
+            //using을 안쓴다고 메모리 누수는 일어나지 않는다. 커넥션 연결의 자동해제를 위해서 using에서 해준다. 안정성을 한다.
+            //socket할 때도 using을 쓰면 클로즈를 자동으로 해준다. 개발자가 close에 집중 안해도 된다.
+            //메모리를 낭비하는 것을 using이 다 처리해준다. 클로즈에 신경쓰지 말자라는 의미로 using을 쓴다.
+            //MRP엔티티프레임워크는 정말 안정성이 좋은 라이브러리이다.
+            return settings;
+        }
+
+        internal static int SetSettings(Settings item)//신규 버튼을 누를 때 클리어 및 코드를 다 쓸 수 있게 손봐줘야 한다.
+        {
+            using (var ctx = new MRPEntities())
+            {
+                ctx.Settings.AddOrUpdate(item);
+                return ctx.SaveChanges();//COMMIT
+            }
+        }
     }
 }
--- MRPApp/MRPApp/View/Setting/SettingList.xaml.cs
+++ MRPApp/MRPApp/View/Setting/SettingList.xaml.cs
@@ -11,6 +11,7 @@ using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace MRPApp.View.Setting
 {
@@ -28,13 +29,21 @@ namespace MRPApp.View.Setting
         {
             try
             {
-                
+                LoadGridData();
+
+                LblBasicCode.Visibility = LblCodeDesc.Visibility = LblCodeName.Visibility = Visibility.Hidden;
             }
             catch (Exception ex)
             {
                 Commons.LOGGER.Error($"예외발생 StoreList Loaded : {ex}");
                 throw ex;
             }
+        }
+
+        private void LoadGridData()
+        {
+            List<Model.Settings> settings = Logic.DataAccess.GetSettings();
+            this.DataContext = settings;
         }
 
         private void BtnEditUser_Click(object sender, RoutedEventArgs e)
@@ -97,9 +106,63 @@ namespace MRPApp.View.Setting
 
         }
 
-        private void BtnUpdate_Click(object sender, RoutedEventArgs e)
+        private async void BtnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            var setting = GrdData.SelectedItem as Model.Settings;//형변환
+            setting.CodeName = TxtCodeName.Text;
+            setting.CodeDesc = TxtCodeDesc.Text;
+
+            try
+            {
+                var result = Logic.DataAccess.SetSettings(setting);
+                if (result == 0)
+                {
+                    Commons.LOGGER.Error("데이터 수정 시 오류발생");
+                    await Commons.ShowMessageAsync("오류", "데이터 수정실패");
+                }
+                else
+                {
+                    Commons.LOGGER.Info($"데이터 수정 성공 : {setting.BasicCode}");//로그
+                    ClearInputs();
+                    LoadGridData();
+                }
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 {ex}");
+            }
+        }
+
+        private void ClearInputs()
+        {
+            TxtBasicCode.IsReadOnly = false;
+            TxtBasicCode.Background = new SolidColorBrush(Colors.White);
+
+            TxtBasicCode.Text = TxtCodeName.Text = TxtCodeDesc.Text = string.Empty;//클리어링
+        }
+
+        private void BtnSearch_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void GrdData_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            try
+            {
+                var setting = GrdData.SelectedItem as Model.Settings;//형변환
+                TxtBasicCode.Text = setting.BasicCode;
+                TxtCodeName.Text = setting.CodeName;
+                TxtCodeDesc.Text = setting.CodeDesc;
+
+                //컨트롤 조작-->BasicCode는 PK이다.
+                TxtBasicCode.IsReadOnly = true;//값을 지우려고 해도 지워지지 않는다.
+                TxtBasicCode.Background = new SolidColorBrush(Colors.LightGray);
+            }
+            catch (Exception ex)
+            {
+                Commons.LOGGER.Error($"예외발생 : {ex}");
+            }
         }
     }
 }
